58380696a1340877224a63ed6934367d1b7a9d12
AhsanAzim <ahsan-azim@live.com>
2017-01-09 16:45:15 -0500

    post component form setup

--- js/post.js
+++ js/post.js
@@ -1,31 +1,119 @@
 class Post extends React.Component {
   constructor() {
     super()
+
+    this.state = {
+      memUsage: "",
+      memAvailable: "",
+      netPacketIn : "",
+      netPacketOut : "",
+      netThroughIn : "",
+      netThroughOut : "",
+      errSys : "",
+      errSen : "",
+      errComp : "",
+      cpuUsage : ""
+    }
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit() {
+    alert('will post to server now');
   }
 
   render() {
     return (
-      <form>
-        <div className="form-group">
-          <label htmlFor="formGroupExampleInput">Example label</label>
-          <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Example input" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="formGroupExampleInput2">Another label</label>
-          <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="formGroupExampleInput2">Another label</label>
-          <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="formGroupExampleInput2">Another label</label>
-          <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="formGroupExampleInput2">Another label</label>
-          <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input" />
-        </div>
+      <form className="form-horizontal" role="form">
+          <div className="form-group">
+           <h1>Post to our server!</h1>
+           <p>Fill in any or all of the fields below, and click submit when you're done!</p>
+          </div>
+          <div className="form-group">
+            <span className="col-md-2 control-label"><h4>CPU Usage</h4></span>
+            <div className="col-md-12">
+              <div className="col-md-3">
+                <input type="text" id="cpuUsageInput" className="form-control" value={this.state.cpuUsage}
+                    onChange={(event) => this.setState({cpuUsage: event.target.value})}/>
+              </div>
+            </div>
+          </div>
+          <div className="form-group">
+              <span className="col-md-2 control-label"><h4>Memory</h4></span>
+              <div className="col-md-12">
+                  <div className="form-group row">
+                      <label htmlFor="memUsageInput" className="col-md-1 control-label">usage</label>
+                      <div className="col-md-3">
+                          <input type="text" id="memUsageInput" className="form-control" value={this.state.memUsage}
+                              onChange={(event) => this.setState({memUsage: event.target.value})}/>
+                      </div>
+                      <label htmlFor="memAvailable" className="col-md-1 control-label">available</label>
+                      <div className="col-md-3">
+                        <input type="text" id="memAvailable" className="form-control" value={this.state.memAvailable}
+                            onChange={(event) => this.setState({memAvailable: event.target.value})}/>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div className="form-group">
+              <span className="col-md-2 control-label"><h4>Network Packets</h4></span>
+              <div className="col-md-12">
+                  <div className="form-group row">
+                      <label htmlFor="netPacketIn" className="col-md-1 control-label">in</label>
+                      <div className="col-md-3">
+                        <input type="text" id="netPacketIn" className="form-control" value={this.state.netPacketIn}
+                            onChange={(event) => this.setState({netPacketIn: event.target.value})}/>
+                      </div>
+                      <label htmlFor="netPacketOut" className="col-md-1 control-label">out</label>
+                      <div className="col-md-3">
+                        <input type="text" id="netPacketOut" className="form-control" value={this.state.netPacketOut}
+                            onChange={(event) => this.setState({netPacketOut: event.target.value})}/>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div className="form-group">
+              <span className="col-md-2 control-label"><h4>Network Throughput</h4></span>
+              <div className="col-md-12">
+                  <div className="form-group row">
+                      <label htmlFor="netThroughIn" className="col-md-1 control-label">in</label>
+                      <div className="col-md-3">
+                        <input type="text" id="netThroughIn" className="form-control" value={this.state.netThroughIn}
+                            onChange={(event) => this.setState({netThroughIn: event.target.value})}/>
+                      </div>
+                      <label htmlFor="netThroughOut" className="col-md-1 control-label">out</label>
+                      <div className="col-md-3">
+                        <input type="text" id="netThroughOut" className="form-control" value={this.state.netThroughOut}
+                            onChange={(event) => this.setState({netThroughOut: event.target.value})}/>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div className="form-group">
+              <span className="col-md-2 control-label"><h4>Errors</h4></span>
+              <div className="col-md-12">
+                  <div className="form-group row">
+                      <label htmlFor="errSys" className="col-md-1 control-label">System</label>
+                      <div className="col-md-3">
+                        <input type="text" id="errSys" className="form-control" value={this.state.errSys}
+                            onChange={(event) => this.setState({errSys: event.target.value})}/>
+                      </div>
+                      <label htmlFor="errSen" className="col-md-1 control-label">Sensor</label>
+                      <div className="col-md-3">
+                        <input type="text" id="errSen" className="form-control" value={this.state.errSen}
+                            onChange={(event) => this.setState({errSen: event.target.value})}/>
+                      </div>
+                      <label htmlFor="errComp" className="col-md-1 control-label">Component</label>
+                      <div className="col-md-3">
+                        <input type="text" id="errComp" className="form-control" value={this.state.errComp}
+                            onChange={(event) => this.setState({errComp: event.target.value})}/>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div className="form-group">
+            <button type="button" onClick={this.onSubmit} className="btn btn-outline-primary">Submit</button>
+          </div>
       </form>
     );
   }
