634217303262ac0e23f64be20e799f65f0648218
AhsanAzim <ahsan-azim@live.com>
2017-01-08 21:00:44 -0500

    multi-lines proof of concept

--- js/chart.js
+++ js/chart.js
@@ -22,6 +22,7 @@ class Chart extends React.Component {
     this.yAxisUnit = this.yAxisUnit.bind(this);
     this.formatData = this.formatData.bind(this);
     this.getNumLines = this.getNumLines.bind(this);
+    this.drawThreeLineGraph = this.drawThreeLineGraph.bind(this);
   }
 
   changeGraph(event) {
@@ -93,9 +94,19 @@ class Chart extends React.Component {
         // graph only when 10 elements present
         if ((self.state.dataQueue[9].length != 0) && !self.state.drawn) {
           self.setState({ drawn: true });
-          self.drawGraph(true);
+          if (self.state.y == 'errors') {
+            self.drawThreeLineGraph(true)
+          } else {
+            self.drawGraph(true);
+          }
+          //self.drawGraph(true);
         } else if ((self.state.dataQueue[9].length != 0) && self.state.drawn && self.state.updateGraph) {
-          self.drawGraph(false);
+          if (self.state.y == 'errors') {
+            self.drawThreeLineGraph(false)
+          } else {
+            self.drawGraph(false);
+          }
+          //self.drawGraph(false);
         }
 
         timer = setTimeout(self.getNext(delay, serverID, timer, requestObj), delay);
@@ -115,9 +126,9 @@ class Chart extends React.Component {
           val3 = d.result.data[0][this.state.y].component
           return {
             date: date,
-            value1: val1,
-            value2: val2,
-            value3: val3
+            val1: val1,
+            val2: val2,
+            val3: val3
           };
           break;
         case 2:
@@ -268,7 +279,153 @@ class Chart extends React.Component {
         .attr("class", "line")
         .attr("d", valueline(data))
         .attr("stroke", this.state.color);
+  }
 
+  drawThreeLineGraph(firstDraw) {
+    var data = this.formatData(this.state.dataQueue);
+    console.log(data);
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var width = 700,   // width of svg
+        height = 400,  // height of svg
+        padding = 100; // space around the chart, not including labels
+
+    var x_domain = d3.extent(data, function(d) { return d.date; }),
+        y_domain = [0, d3.max(data, function(d) { return Math.max(...[d.val1, d.val2, d.val3]); })]
+
+    // display date format
+    var date_format = d3.time.format("%H:%M:%S");
+
+    var vis = null;
+    if (firstDraw) {
+      // create an svg container
+      d3.select(".svgAnchor").select("svg").remove();
+      vis = d3.select(".svgAnchor")
+              .append("svg:svg")
+              .attr("width", width)
+              .attr("height", height);
+    }
+
+    // define the y scale  (vertical)
+    var yScale = d3.scale.linear()
+      .domain(y_domain).nice()   // make axis end in round number
+      .range([height - padding, padding]);   // map these to the chart height, less padding.  In this case 300 and 100
+      //REMEMBER: y axis range has the bigger number first because the y value of zero is at the top of chart and increases as you go down.
+
+
+    var xScale = d3.time.scale()
+      .domain(x_domain)
+      .range([padding, width - padding]);   // map these sides of the chart, in this case 100 and 600
+
+
+    // define the y axis
+    var yAxis = d3.svg.axis()
+        .orient("left")
+        .scale(yScale);
+
+    // define the x axis
+    var xAxis = d3.svg.axis()
+        .orient("bottom")
+        .scale(xScale)
+        .tickFormat(date_format);
+
+    // Define the line
+    var valueline = d3.svg.line()
+        .x(function(d) { return xScale(d.date.getTime()); })
+        .y(function(d) { return yScale(d.val1); })
+        .interpolate("basis");
+
+    var valueline2 = d3.svg.line()
+        .x(function(d) { return xScale(d.date.getTime()); })
+        .y(function(d) { return yScale(d.val2); })
+        .interpolate("basis");
+
+    var valueline3 = d3.svg.line()
+        .x(function(d) { return xScale(d.date.getTime()); })
+        .y(function(d) { return yScale(d.val3); })
+        .interpolate("basis");
+
+    if (!firstDraw) {
+      // Select the section we want to apply our changes to
+      var svg = d3.select(".svgAnchor").transition();
+
+      // Make the changes
+      svg.select(".line1")   // change the line
+          .duration(750)
+          .attr("d", valueline(data))
+          .attr("stroke", this.state.color);
+      svg.select(".line2")   // change the line
+          .duration(750)
+          .attr("d", valueline2(data))
+          .attr("stroke", this.state.color);
+      svg.select(".line3")   // change the line
+          .duration(750)
+          .attr("d", valueline3(data))
+          .attr("stroke", this.state.color);
+      svg.select(".xaxis") // change the x axis
+          .duration(750)
+          .attr("transform", "translate(0," + (height - padding) + ")")
+          .call(xAxis);
+      svg.select(".yaxis") // change the y axis
+          .duration(750)
+          .call(yAxis);
+
+      svg.selectAll(".xaxis text")  // select all the text elements for the xaxis
+        .attr("transform", function(d) {
+           return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height + ")rotate(-45)";
+       });
+
+      return
+    }
+
+    // draw y axis with labels and move in from the size by the amount of padding
+    vis.append("g")
+        .attr("class", "yaxis axis")
+        .attr("transform", "translate("+padding+",0)")
+        .call(yAxis);
+    // draw x axis with labels and move to the bottom of the chart area
+    vis.append("g")
+        .attr("class", "xaxis axis")  // two classes, one for css formatting, one for selection below
+        .attr("transform", "translate(0," + (height - padding) + ")")
+        .call(xAxis);
+
+    // now rotate text on x axis
+    // solution based on idea here: https://groups.google.com/forum/?fromgroups#!topic/d3-js/heOBPQF3sAY
+    // first move the text left so no longer centered on the tick
+    // then rotate up to get 45 degrees.
+    vis.selectAll(".xaxis text")  // select all the text elements for the xaxis
+      .attr("transform", function(d) {
+         return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height + ")rotate(-45)";
+     });
+
+    // now add titles to the axes
+    vis.append("text")
+        .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+        .attr("transform", "translate("+ (padding/2) +","+(height/2)+")rotate(-90)")  // text is drawn off the screen top left, move down and out and rotate
+        .text(`${this.state.y.replace(/_/g, " ")} (${this.yAxisUnit(this.state.y)})`);
+    vis.append("text")
+        .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+        .attr("transform", "translate("+ (width/2) +","+(height-(padding/3))+")")  // centre below axis
+        .text("Time");
+
+    // Add the valueline path.
+    vis.append("path")
+        .attr("class", "line line1")
+        .attr("d", valueline(data))
+        .attr("stroke", this.state.color);
+
+    vis.append("path")
+        .attr("class", "line line2")
+        .attr("d", valueline2(data))
+        .attr("stroke", "green");
+
+    vis.append("path")
+        .attr("class", "line line3")
+        .attr("d", valueline3(data))
+        .attr("stroke", "pink");
   }
 
   render() {
@@ -286,6 +443,7 @@ class Chart extends React.Component {
               <option value="memory_usage">memory usage</option>
               <option value="memory_available">memory available</option>
               <option value="cpu_usage">cpu usage</option>
+              <option value="errors">errors</option>
             </select>
           </div>
           <div className="form-group">
